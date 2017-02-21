f210c13bcc3b42d2f85dcdfb6b8f7786cad73d1a
Mihai Costiug <mihai.co21@gmail.com>
2017-02-21 16:09:44 +0200

    Updated CoreData model + implemented save and increment functionality.

--- SmartDialer/SmartDialer/RecentCallsManager.swift
+++ SmartDialer/SmartDialer/RecentCallsManager.swift
@@ -13,24 +13,98 @@ import CoreData
 class RecentCallsManager: NSObject {
     static let sharedInstance = RecentCallsManager()
     
-    private var callCounters: [NSManagedObject] = []
+    private var callCounters: [CallCounter] = []
     
     private override init() {
         super.init()
         
         self.fetchAllCallCounters()
+        self.addObservers()
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
     
     //MARK:- Public
     
     public func incrementCounter(phoneNumber: String) {
-        
+        DispatchQueue.global(qos: .background).async {
+            var alreadyExists = false
+            
+            for callCounter in self.callCounters {
+                if callCounter.phoneNumber == phoneNumber {
+                    callCounter.callCount += 1
+                    callCounter.lastCallDate = NSDate()
+                    
+                    alreadyExists = true
+                    break
+                }
+            }
+            
+            if !alreadyExists {
+                let context = self.persistentContainer.viewContext
+                let callCounter = CallCounter(context: context)
+                callCounter.phoneNumber = phoneNumber
+                callCounter.callCount = 1
+                callCounter.lastCallDate = NSDate()
+                context.insert(callCounter)
+            }
+            
+            self.saveContext()
+        }
     }
     
     //MARK:- Private
     
     private func fetchAllCallCounters() {
+        DispatchQueue.global(qos: .background).async {
+            do {
+                let context = self.persistentContainer.viewContext
+                self.callCounters = try context.fetch(CallCounter.fetchRequest())
+            } catch {
+                print("Fetching call counters failed")
+            }
+        }
+    }
+    
+    private func addObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(applicationWillTerminateNotification(notification:)), name: NSNotification.Name.UIApplicationWillTerminate, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(applicationDidEnterBackgroundNotification(notification:)), name: NSNotification.Name.UIApplicationDidEnterBackground, object: nil)
+    }
+    
+    //MARK:- Notifications
+    
+    func applicationWillTerminateNotification(notification: NSNotification) {
+        self.saveContext()
+    }
+    
+    func applicationDidEnterBackgroundNotification(notification: NSNotification) {
         
     }
     
+    //MARK:- Core Data stack
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "CallCounterModel")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                
+            }
+        })
+        return container
+    }()
+    
+    //MARK:- Core Data Saving support
+    
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                
+            }
+        }
+    }
 }
